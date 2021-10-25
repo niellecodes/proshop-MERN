96abd5e1b7ae3a659c7c2679c1e02e3383c4289d
Seun <ojoseun10@yahoo.com>
2021-10-25 10:32:53 +0100

    new changes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,36 +1,12 @@
 import express from "express"
-import asyncHandler from "express-async-handler"
-
 const router = express.Router()
-import Product from "../models/productModel.js"
+import {getProductById,getProducts} from "../controllers/productController.js"
 
-
-//@desc fetch all products
-//@route  GET /api/products
-//@access public route    
-router.get("/", asyncHandler(async (req,res) => {
-    const products = await Product.find({})
-    res.json(products)
-}))
-
-
-//@desc fetch single product
-//@route  GET /api/products/:id
-//@access public route  
-router.get(
-    "/:id",
-     asyncHandler(async (req,res) => {
-    const product = await Product.findById(req.params.id)
-
-    if (product) {
-        res.json(product)
-    } else {
-        res.status(404)
-        throw new Error ("Product not found")
-    }
 
   
-})
-)
+router.route("/").get(getProducts)
+
+router.route("/:id").get(getProductById)
+
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,15 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
 import connectDB from "./config/db.js"
 
 import productRoutes from "./routes/productRoutes.js"
-
+import userRoutes from "./routes/userRoutes.js"
 
 dotenv.config()
 
 connectDB()
 
 const app = express()
+
+app.use(express.json())
 
 
 
@@ -20,7 +22,7 @@ app.get("/", (req,res) =>{
 })
 
 app.use("/api/products", productRoutes)
-
+app.use("/api/users", userRoutes)
 app.use(notFound)
 
 app.use(errorHandler)
